01be046650fb6ecb37a6f0184e1f47f95ac8297c
KANCHAN PATEL <38435500+kp1developer@users.noreply.github.com>
2022-07-14 17:58:05 +0530

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+      id: 1,
+      name: 'Swati',
+      email: 'swati@gmail.com',
+      status: 'active'
+    },
+    {
+      id: 2,
+      name: 'Sachin Patel',
+      email: 'sachinpatel@gmail.com',
+      status: 'inactive'
+    },
+    {
+      id: 3,
+      name: 'Shailendra Singh Patel',
+      email: 'shailendrasingh@gmail.com',
+      status: 'active'
+    }
+  ];
+  
+  module.exports = members;
